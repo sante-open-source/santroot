2d288c9a7958086fc98be4cdd5268bfeb823e1ff
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-04-12 14:21:53 +0800

    Configuring list on toolchain

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 wget -q -O- https://toolchains.bootlin.com/downloads/releases/toolchains/x86-64/tarballs/x86-64--glibc--bleeding-edge-2022.08-1.tar.bz2 | tar -xjf- -C /opt
-ls /opt
+ls /opt/x86-64--glibc--bleeding-edge-2022.08-1
