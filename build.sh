d0ea3f83ab62efdd515406ef1f0ef98c4d17a3eb
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-04-12 18:24:40 +0800

    Using a file for corresponding fixtires

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export PATH="$PATH:/opt/x86-64--glibc--bleeding-edge-2022.08-1/bin"
 # Build a santroot
 # 1. m4
 wget -q -O- https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.xz | tar -xJf-
-cd m4-1.4.19
+cd m4*
 mkdir build
 pushd build
   ../configure --prefix=/usr \
@@ -20,10 +20,10 @@ cd ..
 rm -rf m4*
 # 2. ncurses
 git clone https://github.com/ThomasDickey/ncurses-snapshots ncurses
-cd ncurses
+cd ncurses*
 sed -i s/mawk// configure
-mkdir build-tic
-pushd build-tic
+mkdir build
+pushd build
   ../configure --silent
   make --silent -C include
   make --silent -C progs tic
@@ -42,7 +42,7 @@ popd
 	    --enable-widec \
 	    --silent
 make --silent
-make --silent DESTDIR=/opt/santroot TIC_PATH=$(pwd)/build-tic/progs/tic install
+make --silent DESTDIR=/opt/santroot TIC_PATH=$(pwd)/build/progs/tic install
 echo "INPUT(-lncursesw)" > /opt/santroot/usr/lib/libncurses.so
 cd ..
 rm -rf ncurses*
@@ -63,19 +63,49 @@ cd ..
 rm -rf dash*
 # 4. coreutils
 wget -q -O- https://ftp.gnu.org/gnu/coreutils/coreutils-9.2.tar.xz | tar -xJf-
-cd coreutils-9.2
+cd coreutils-*
 mkdir build
 pushd build
   ../configure --prefix=/usr \
 	       --libexecdir=/usr/lib \
-	       --host=$LFS_TGT \
+	       --host=x86_64-buildroot-linux-gnu \
 	       --build=$(../build-aux/config.guess) \
+	       --silent \
 	       FORCE_UNSAFE_CONFIGURE=1
   make --silent
   make --silent DESTDIR=/opt/santroot install
 popd
 cd ..
 rm -rf coreutils*
-# 6. diffutils
-
+# 5. diffutils
+wget -q -O- https://ftp.gnu.org/gnu/diffutils/diffutils-3.9.tar.xz | tar -xJf-
+cd diffutils*
+mkdir build
+pushd build
+  ../configure --prefix=/usr \
+	       --host=x86_64-buildroot-linux-gnu \
+	       --silent
+  make --silent
+  make --silent DESTDIR=/opt/santroot install
+popd
+cd ..
+rm -rf diffutils*
+# 6. file
+git clone https://github.com/file/file
+cd file*
+autoreconf -fi
+mkdir build
+pushd build
+  ../configure --disable-bzlib \
+               --disable-libseccomp \
+               --disable-xzlib \
+               --disable-zlib \
+	       --silent
+popd
+./configure --prefix=/usr \
+	    --host=$LFS_TGT \
+	    --build=$(./config.guess) \
+	    --silent
+make --silent FILE_COMPILE=$(pwd)/build/src/file
+make --silent DESTDIR=/opt/santroot install
 ls /opt/santroot
