8c418ce9b085e8cedd9ec0e04e22b6af61b70fb9
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-04-12 14:48:25 +0800

    Using a limit used

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -e
 wget -q -O- https://toolchains.bootlin.com/downloads/releases/toolchains/x86-64/tarballs/x86-64--glibc--bleeding-edge-2022.08-1.tar.bz2 | tar -xjf- -C /opt
-ls /opt/x86-64--glibc--bleeding-edge-2022.08-1
+export PATH="$PATH:/opt/x86-64--glibc--bleeding-edge-2022.08-1/bin"
+git clone git://git.sv.gnu.org/m4.git
+cd m4
+./bootstrap
+mkdir build
+pushd build
+  ../configure --prefix=/usr \
+	       --host=x86_64-buildroot-linux-gnu \
+	       --build=$(../build-aux/config.guess)
+  make
+  make DESTDIR=/opt/santroot
+popd
