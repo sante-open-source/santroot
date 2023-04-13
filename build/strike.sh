1a7c3e9f247b0332fc283350b13d81d20fe12fe1
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-04-13 14:52:46 +0800

    Use build sysroot on build

--- build/strike.sh
+++ build/strike.sh
@@ -16,3 +16,29 @@ pushd build
 popd
 cd ..
 rm -rf binutils*
+
+git clone https://gcc.gnu.org/git/gcc
+cd gcc*
+./contrib/download_prequisites
+mkdir build
+pushd build
+  ../configure --build=$(../config.guess) \
+	       --host=x86_64-buildroot-linux-gnu \
+	       --target=x86_64-buildroot-linux-gnu \
+	       LDFLAGS_FOR_TARGET=-L$PWD/x86_64-buildroot-linux-gnu/libgcc \
+	       --prefix=/usr \
+	       --with-build-sysroot=/opt/x86-64--glibc--bleeding-edge-2022.08-1/x86_64-buildroot-linux-gnu \
+	       --enable-default-pie \
+	       --enable-default-ssp \
+	       --disable-multilib \
+	       --disable-libatomic \
+	       --disable-libgomp \
+	       --disable-libquadmath \
+	       --disable-libssp \
+	       --disable-libvtv \
+	       --enable-languages=c,c++
+  make --silent
+  make --silent DESTDIR=/opt/santroot install
+popd
+cd ..
+rm -rf gcc*
