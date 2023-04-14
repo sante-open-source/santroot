5aab6e84d315bb60529190cce17a81c8bec25b89
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-04-14 19:36:10 +0800

    Use the build via exclude

--- build/strike.sh
+++ build/strike.sh
@@ -17,20 +17,20 @@ popd
 cd ..
 rm -rf binutils*
 
-git clone https://gcc.gnu.org/git/gcc.git
+wget -q -O- https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz | tar -xJf-
 cd gcc*
 sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
 sed '/thread_header =/s/@.*@/gthr-posix.h/' -i libgcc/Makefile.in libstdc++-v3/include/Makefile.in
 ./contrib/download_prerequisites
 mkdir build
 pushd build
-  # LDFLAGS_FOR_TARGET=-L$PWD/x86_64-buildroot-linux-gnu/libgcc
   ../configure --build=$(../config.guess) \
 	       --host=x86_64-buildroot-linux-gnu \
 	       --target=x86_64-buildroot-linux-gnu \
 	       --prefix=/usr \
 	       --libdir=/usr/lib \
 	       --libexecdir=/usr/lib \
+	       LDFLAGS_FOR_TARGET=-L$PWD/x86_64-buildroot-linux-gnu/libgcc \
 	       --with-build-sysroot=/opt/x86-64--glibc--bleeding-edge-2022.08-1/x86_64-buildroot-linux-gnu/sysroot \
 	       --enable-default-pie \
 	       --enable-default-ssp \
